d12b663cd4a20db0a06b2d780a50a618b65acc76
Nicolas Posa <nicoposa57@gmail.com>
2023-05-10 00:52:26 -0300

    first chapter array

---
+++ arrays.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// array
+// conjunto
+// diccionario
+
+//ARRAYS
+var someInts = [Int]()
+someInts.count
+someInts.append(23)
+someInts.count
+someInts = []
+someInts.count
+
+var someDobles = Array(repeating: 3.14, count: 7)
+someDobles.count
+
+var moreDobles = Array(repeating: 2.5, count: 4)
+
+var alotDobles = someDobles + moreDobles
+alotDobles.count
+ 
+var shoppingList : [String] = ["papa", "pimientos", "tortillas", "queso"]
+shoppingList.count
+
