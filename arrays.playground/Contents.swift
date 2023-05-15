345268d3de7465a59a7cfab20f48b997b7d10e53
Nicolas Posa <nicoposa57@gmail.com>
2023-05-14 23:24:05 -0300

    arrays

--- arrays.playground/Contents.swift
+++ arrays.playground/Contents.swift
@@ -23,3 +23,26 @@ alotDobles.count
 var shoppingList : [String] = ["papa", "pimientos", "tortillas", "queso"]
 shoppingList.count
 
+if shoppingList.isEmpty{
+    print("esta vacia")
+} else {
+    print("Ricardo va a comprar")
+}
+
+shoppingList.append("Coca Cola")
+
+shoppingList += ["Palta"]
+
+var firsElement = shoppingList[0]
+shoppingList[0] = "Manteca"
+shoppingList[0...4]
+
+let removeManteca = shoppingList.remove(at: 0)
+shoppingList
+
+shoppingList.removeLast()
+shoppingList
+
+for item in shoppingList{
+    print(item)
+}
