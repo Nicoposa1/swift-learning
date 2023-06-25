a932d7ffcb555bae1b3a85ea5b4720a1a5b6e737
Nicolas Posa <nicoposa57@gmail.com>
2023-06-25 04:33:56 -0300

    returns values

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -21,3 +21,40 @@ func greeting (person: String, isMale: Bool) -> String {
 
 greeting(person: "Nicolás", isMale: true)
 greeting(person: "Lucia", isMale: false)
+
+func anotherGreet(person: String) {
+    print("Hola \(person)")
+}
+anotherGreet(person: "Lucia")
+
+func printAndCount(string: String) -> Int {
+    print(string)
+    return string.count
+}
+printAndCount(string: "Hola a todos")
+
+func printWithoutCount(string: String) {
+    print(string)
+}
+printWithoutCount(string: "Hola Que tal")
+
+func minMax (array:[Int]) -> (min: Int, max: Int)?{
+    
+    if array.isEmpty{return nil}
+    
+    var minValue = array[0]
+    var maxValue = array[0]
+    for value in array[1..<array.count] {
+        if value < minValue {
+            minValue = value
+        } else if value > maxValue {
+            maxValue = value
+        }
+    }
+    
+    return(minValue, maxValue)
+}
+
+let bounds = minMax(array: [3, 5, -10, 12])
+print("Los valores se encuentra entre \(bounds!.min) y \(bounds!.max)")
+ 
