5b468973a6c611de361963be5bbddae1d0252087
Nicolas Posa <nicoposa57@gmail.com>
2023-06-29 00:02:19 -0300

    capture values

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -15,3 +15,43 @@ reversedName = name.sorted(by: { s1, s2 in s1 > s2 })
 reversedName = name.sorted(by: { (s1: String, s2: String) in
     return s1 > s2
 })
+
+func someFunctionThatTakesAClosuresure(closure: () -> Void) {
+    // aquí ira el cuerpo de la funcion
+}
+
+someFunctionThatTakesAClosuresure (closure: {
+    // aquí ira el cuerpo de la funcionÏ
+})
+
+someFunctionThatTakesAClosuresure{
+    // añadir el closure here
+}
+
+reversedName = name.sorted{$0>$1}
+
+let digitNames = [0: "cero", 1: "uno", 2:"dos", 3:"tres", 4: "cuatro", 5:"cinco", 6: "seis", 7:"siete", 8:"ocho", 9:"nueve"]
+
+let numbers = [14, 113, 64, 933]
+let numberStrings = numbers.map{(number) -> String in
+    var number = number
+    var output = ""
+    repeat{
+        output = digitNames[number%10]! + output
+        number /= 10
+    } while number > 0
+            return output
+}
+
+func makeIncrementer (forIncrement amount : Int) -> () -> Int {
+    var runningTotal = 0
+    func incrementer () -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    return incrementer
+}
+
+let incrementeByTen = makeIncrementer(forIncrement: 10)
+incrementeByTen()
+incrementeByTen()
