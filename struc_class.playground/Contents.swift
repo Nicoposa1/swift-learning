3d137ec58ce99c69869b633644011ca9b42e05a5
Nicolas Posa <nicoposa57@gmail.com>
2023-07-04 09:59:25 -0300

    definition struct and class

--- struc_class.playground/Contents.swift
+++ struc_class.playground/Contents.swift
@@ -1,6 +1,30 @@
 import UIKit
 
-class {
-prueba: String
-tests: Numeric
+//Struc vs Class
+
+struct SomeStruct {
+    //la definición de la estructura aquí
 }
+
+class SomeClass {
+    //La definición de la clase aquí
+}
+
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+
+let someResolution = Resolution()
+let someVideoMode = VideoMode()
+
+print(someResolution.width)
+someVideoMode.resolution.width = 1280
+print(someVideoMode.resolution.width)
