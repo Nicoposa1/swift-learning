7e86ef4231b6e687ac2991b04a34fd72c5748e0d
Nicolas Posa <nicoposa57@gmail.com>
2024-07-02 01:21:33 -0300

    mutating methods

--- Methods.playground/Contents.swift
+++ Methods.playground/Contents.swift
@@ -27,7 +27,36 @@ struct Point {
     func isToTheRigth(of x:Double) -> Bool {
         return self.x > x
     }
+    
+    mutating func moveBy(x deltaX: Double, y deltaY:Double) {
+        x += deltaX
+        y += deltaY
+    }
 }
 
-let somePoint = Point(x: 4)
+var somePoint = Point(x: 4)
 somePoint.isToTheRigth(of: 1)
+
+somePoint.moveBy(x: 13, y: 20)
+somePoint.x = 9
+
+
+enum DifferentStateSwitch {
+    case off, low, high
+    
+    mutating func next(){
+        switch self {
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
+}
+
+
+var controllerStatus = DifferentStateSwitch.off
+controllerStatus.next()
+controllerStatus.next()
