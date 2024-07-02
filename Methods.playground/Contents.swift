2b5720bf088bd8bcf396d937358f98727e0044f5
Nicolas Posa <nicoposa57@gmail.com>
2024-07-02 00:24:10 -0300

    metodos de instancia

--- Methods.playground/Contents.swift
+++ Methods.playground/Contents.swift
@@ -3,53 +3,31 @@ import UIKit
 class Counter {
     var count = 0
     
-    func increment (){
+    func increment(){
         self.count += 1
     }
     
-    func increment (by amount: Int){
+    func increment(by amount:Int) {
         self.count += amount
     }
-    func reset () {
+    
+    func reset(){
         self.count = 0
     }
 }
 
 let counter = Counter()
+
 counter.increment()
-counter.increment(by: 12)
+counter.increment(by: 9)
+counter.reset()
 
 struct Point {
     var x = 0.0, y = 0.0
-    func isToTheRigth(of x: Double) -> Bool {
+    func isToTheRigth(of x:Double) -> Bool {
         return self.x > x
     }
-    
-    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
-        //x += deltaX
-        //y += deltaY
-        self = Point(x: self.x + deltaX, y: self.y + deltaY)
-        
-    }
 }
 
-var somePoint = Point(x: 4, y: 3)
-somePoint.isToTheRigth(of: 3)
-somePoint.moveBy(x: 2, y: -3)
-
-enum SwithCase {
-    case off, low, high
-    mutating func next() {
-        switch self{
-        case .off:
-            self = .low
-        case .low:
-            self = .high
-        case .high:
-            self = .off
-        }
-    }
-}
-
-var controller = SwithCase.off
-controller.next()
+let somePoint = Point(x: 4)
+somePoint.isToTheRigth(of: 1)
