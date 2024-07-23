300f9f0fab2ce2cf7986bc9a4fb74482a05a2396
Nicolas Posa <nicoposa57@gmail.com>
2024-07-23 00:45:09 -0300

    methods

--- Methods.playground/Contents.swift
+++ Methods.playground/Contents.swift
@@ -60,3 +60,60 @@ enum DifferentStateSwitch {
 var controllerStatus = DifferentStateSwitch.off
 controllerStatus.next()
 controllerStatus.next()
+
+
+class SomeClass {
+    class func someMethod(){
+        print("Hola")
+    }
+}
+
+SomeClass.someMethod()
+
+
+struct LevelTracker{
+    static var highestUnlockedLevel = 1
+    var currentLevel = 1
+    static func unlock (_ level:Int){
+        if level > highestUnlockedLevel {
+            highestUnlockedLevel = level
+        }
+    }
+    
+    static func unlocked (_ level: Int) -> Bool {
+         return level <= highestUnlockedLevel
+    }
+    
+    mutating func advance(to level: Int) -> Bool {
+        if LevelTracker.unlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Player {
+    var tracker = LevelTracker()
+    let playerName:String
+    func complete(level: Int) {
+        LevelTracker.unlock(level + 1)
+        tracker.advance(to: level + 1)
+    }
+    init(name: String) {
+        self.playerName = name
+    }
+}
+
+var player = Player(name: "Nicolas")
+player.complete(level: 1)
+player
+
+if player.tracker.advance(to: 7){
+    print("Podemos avanzar al nivel 7")
+} else {
+    print("El nive 7 no esta desbloqueado")
+}
+
+
