3d033dd1f7835bbf77e99a5f945ad6581cdf2622
Nicolas Posa <nicoposa57@gmail.com>
2024-07-25 22:24:14 -0300

    matrix with sbuscript

--- subscripts.playground/Contents.swift
+++ subscripts.playground/Contents.swift
@@ -24,3 +24,37 @@ enum Planet: Int {
 }
 
 let mars = Planet[4]
+
+
+struct Matrix {
+    let rows:Int, columns: Int
+    var grid: [Double]
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows*columns)
+    }
+    
+    func indexIsValid(row:Int, column:Int) -> Bool {
+        return row >= 0 && column >= 0 && row < rows && column <  columns
+    }
+    
+    
+    subscript(row:Int, column:Int) -> Double {
+        get{
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set{
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column] = newValue
+        }
+    }
+    
+}
+
+var matrix = Matrix(rows: 15, columns: 15)
+matrix[0, 1] = 1.5
+matrix[1, 0] = 2.5
+matrix
