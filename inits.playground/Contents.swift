c1f3b0dd481ebf36e15ca59236f67e4a3ca352a7
Nicolas Posa <nicoposa57@gmail.com>
2024-07-30 23:36:23 -0300

    init

---
+++ inits.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+struct Fahrenheit {
+    var temperature: Double
+    init() {
+        self.temperature = 32
+    }
+}
+
+var f1 = Fahrenheit()
+
+struct Celcius {
+    var temperature: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        self.temperature = (fahrenheit - 32) / 1.8
+    }
+    init(fromKelvin kelvin:Double) {
+        self.temperature = kelvin - 273.15
+    }
+}
+
+var boilingPointOfWater = Celcius(fromFahrenheit: 212)
+var freezingPointOfWater = Celcius(fromKelvin: 273.15)
