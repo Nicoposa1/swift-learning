9ec3833687ff6d408f549e9e4a13683eb8f6d3c7
Nicolas Posa <nicoposa57@gmail.com>
2024-08-19 23:48:27 -0300

    optional chaining

--- optional_chaining.playground/Contents.swift
+++ optional_chaining.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class Person {
+/*class Person {
     var residence: Residence?
 }
 
@@ -22,4 +22,50 @@ if let roomCount = Edgar.residence?.numberOfRooms {
     print("La casa de Edgar tiene \(roomCount) habitaciones")
 } else {
     print("Edgar no tiene casa")
+}*/
+
+class Person {
+    var residence: Residence?
 }
+
+class Residence {
+    var rooms = [Room]()
+    var numberOfRomms: Int {
+        return rooms.count
+    }
+    subscript(i:Int) -> Room {
+        get{
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+}
+
+class Room {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+            return "\(buildingNumber), \(street)"
+        }else if buildingName != nil {
+            return buildingName
+        } else {
+            return nil
+        }
+    }
+}
+
+
+
+
